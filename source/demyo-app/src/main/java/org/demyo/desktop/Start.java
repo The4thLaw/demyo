52f9002dae01f431143565ef076c2d5e5e21d339
The4thLaw <The4thLaw@users.noreply.github.com>
2015-06-01 09:05:55 +0200

    Adding first tray icon functionnality to exit Demyo.

--- source/demyo-app/src/main/java/org/demyo/desktop/Start.java
+++ source/demyo-app/src/main/java/org/demyo/desktop/Start.java
@@ -1,6 +1,18 @@
 package org.demyo.desktop;
 
+import java.awt.AWTException;
+import java.awt.MenuItem;
+import java.awt.PopupMenu;
+import java.awt.SystemTray;
+import java.awt.TrayIcon;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
+import java.net.URL;
+
+import javax.swing.ImageIcon;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
 
 import org.demyo.model.config.SystemConfiguration;
 
@@ -42,7 +54,7 @@ public class Start {
 
 		LOGGER.info("Starting server...");
 
-		Server server = new Server(SystemConfiguration.getInstance().getHttpPort());
+		final Server server = new Server(SystemConfiguration.getInstance().getHttpPort());
 
 		WebAppContext webapp = new WebAppContext();
 		webapp.setContextPath("/");
@@ -51,7 +63,64 @@ public class Start {
 		server.setHandler(webapp);
 
 		server.start();
+
+		if (SystemTray.isSupported()) {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+
+			SwingUtilities.invokeLater(new Runnable() {
+				@Override
+				public void run() {
+					createAndShowGUI(server);
+				}
+
+			});
+		} else {
+			LOGGER.info("System tray is not supported by the current system; there will be no GUI to close Demyo");
+		}
+
 		LOGGER.info("Demyo is now ready");
 		server.join();
+		LOGGER.debug("Main method exiting");
+		// This should not be needed, but the EDT hangs on.
+		System.exit(0);
+	}
+
+	private static void createAndShowGUI(final Server server) {
+		final PopupMenu popup = new PopupMenu();
+		MenuItem exitItem = new MenuItem("Exit Demyo");
+		exitItem.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				LOGGER.info("Stopping Demyo");
+				for (TrayIcon icon : SystemTray.getSystemTray().getTrayIcons()) {
+					SystemTray.getSystemTray().remove(icon);
+				}
+				// This should not be needed, but the EDT is still running :/
+				/*for (Window w : Window.getWindows()) {
+					LOGGER.debug("Disposing of window {}", w);
+					w.dispose();
+				}*/
+
+				try {
+					server.stop();
+				} catch (Exception ex) {
+					LOGGER.warn("Error while stopping server", ex);
+				}
+			}
+		});
+		popup.add(exitItem);
+
+		URL iconUrl = Start.class.getResource("app-icon.png");
+		assert (iconUrl != null);
+		ImageIcon icon = new ImageIcon(iconUrl, "Demyo tray icon");
+		TrayIcon trayIcon = new TrayIcon(icon.getImage(), "Demyo");
+		trayIcon.setPopupMenu(popup);
+		try {
+			SystemTray.getSystemTray().add(trayIcon);
+		} catch (AWTException e) {
+			LOGGER.warn("Failed to add a system tray icon; there will be no GUI to close Demyo");
+		}
+
+		LOGGER.info("System tray menu is available");
 	}
 }
