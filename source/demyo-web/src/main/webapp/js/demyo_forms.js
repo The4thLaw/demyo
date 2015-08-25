b1f6740e1989e1f54a8cffeefde17a2bcbf8e546
The4thLaw <The4thLaw@users.noreply.github.com>
2015-08-25 19:09:59 +0200

    Add a way to clear colour inputs. Preview the tag final look.

--- source/demyo-web/src/main/webapp/js/demyo_forms.js
+++ source/demyo-web/src/main/webapp/js/demyo_forms.js
@@ -5,16 +5,29 @@
 
 (function($) {
 
-	/*
+	/**
 	 * Clears validation errors on change.
 	 * This is broken for TinyMCE but validation on such field is currently not performed in Demyo
 	 */
 	$(function() {
-		$('form .value .error').change(function() {
-			self = $(this);
+		$('form .value .error').change(function () {
+			var self = $(this);
 			self.removeClass('error');
 			$('.validation-error', self.parents('.value').get(0)).remove();
 		});
+	});
+	
+	/**
+	 * Clears the colour inputs on demand
+	 */
+	$(function() {
+		$('.colour-remover input').change(function (e) {
+			var self = $(this);
+			$('#' + self.data('for')).prop('disabled', self.is(':checked'));
+			$('#' + self.data('for')).change(); // Trigger event
+		});
+		// Trigger at start to disable the input if needed
+		$('.colour-remover input').change();
 	});
 	
 })(jQuery);
