812a637defed26d1a703ba53727b448e2ca2a99d
The4thLaw <The4thLaw@users.noreply.github.com>
2015-09-13 12:29:06 +0200

    - Use our local subinterface for repositories - Fix textList macro

--- source/demyo-dao/src/main/java/org/demyo/dao/IBindingRepo.java
+++ source/demyo-dao/src/main/java/org/demyo/dao/IBindingRepo.java
@@ -2,13 +2,12 @@ package org.demyo.dao;
 
 import org.demyo.model.Binding;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 /**
  * This class provides methods to manipulate {@link Binding}s.
  */
 @Repository
-public interface IBindingRepo /*extends IModelRepo<Binding>*/extends JpaRepository<Binding, Long> {
+public interface IBindingRepo extends IModelRepo<Binding> {
 
 }
