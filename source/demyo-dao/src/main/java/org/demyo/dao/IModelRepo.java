b5dd966b7e2ad25ca077ca12e276ff41380c667f
The4thLaw <The4thLaw@users.noreply.github.com>
2015-09-15 17:18:07 +0200

    Return Slices rather than Pages.

--- source/demyo-dao/src/main/java/org/demyo/dao/IModelRepo.java
+++ source/demyo-dao/src/main/java/org/demyo/dao/IModelRepo.java
@@ -1,17 +1,30 @@
 package org.demyo.dao;
 
+import java.util.List;
+
 import org.demyo.model.IModel;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.NoRepositoryBean;
 
 /**
- * Base interface for manipulation of models.
+ * Base interface for manipulation of models. This doesn't inherit from JpaRepository, in order to be free to
+ * define exactly what we need.
  * 
  * @param <M> The model type (an {@link IModel}).
  */
 @NoRepositoryBean
-public interface IModelRepo<M extends IModel> extends JpaRepository<M, Long> {
-	// TODO: figure out how to return slices. We can't override JpaRepository's findAll to return a slice
-	//Slice<M> findAll(Pageable pageable);
+public interface IModelRepo<M extends IModel> extends CrudRepository<M, Long> {
+	@Override
+	List<M> findAll();
+
+	/**
+	 * Returns a {@link Slice} of entities meeting the paging restriction provided in the {@code Pageable} object.
+	 * 
+	 * @param pageable
+	 * @return a page of entities
+	 */
+	Slice<M> findAll(Pageable pageable);
 }
