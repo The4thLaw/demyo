af10a8e8ce291a590d8af023252d214510cee1a4
The4thLaw <The4thLaw@users.noreply.github.com>
2015-10-17 07:15:19 +0200

    Fixed comparator based on identifying names. It used to always use the fallback ordering.

--- source/demyo-model/src/main/java/org/demyo/model/util/IdentifyingNameComparator.java
+++ source/demyo-model/src/main/java/org/demyo/model/util/IdentifyingNameComparator.java
@@ -11,6 +11,9 @@ public class IdentifyingNameComparator extends AbstractModelComparator<IModel> {
 	@Override
 	public int compare(IModel m1, IModel m2) {
 		int comparison = nullSafeCollatingComparison(m1.getIdentifyingName(), m2.getIdentifyingName());
+		if (comparison != 0) {
+			return comparison;
+		}
 		return defaultComparison(m1, m2);
 	}
 }
