f961774d0f21881b0c32a9601d4cb1abd40b7fd6
The4thLaw <The4thLaw@users.noreply.github.com>
2016-07-06 12:10:03 +0200

    Added a test for number formatting in albums.
    Refers to commit 985b825e65dcf0949a4f6ccd5b0f39d2f528b11a .

---
+++ source/demyo-model/src/test/java/org/demyo/model/AlbumTest.java
@@ -0,0 +1,30 @@
+package org.demyo.model;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.math.BigDecimal;
+
+import org.junit.Test;
+
+/**
+ * Tests for {@link Album}.
+ */
+public class AlbumTest {
+	/**
+	 * Tests number formatting in {@link Album#getIdentifyingName()}. Relates to commit
+	 * 985b825e65dcf0949a4f6ccd5b0f39d2f528b11a.
+	 */
+	@Test
+	public void testNumberFormatting() {
+		Album album = new Album();
+		album.setTitle("Title");
+		album.setNumber(BigDecimal.valueOf(1));
+		assertThat(album.getIdentifyingName()).isEqualTo("1 - Title");
+
+		album.setCycle(0);
+		assertThat(album.getIdentifyingName()).isEqualTo("0.1 - Title");
+
+		album.setNumber(BigDecimal.valueOf(1.2d));
+		assertThat(album.getIdentifyingName()).isEqualTo("0.1.2 - Title");
+	}
+}
