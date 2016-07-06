0d693a350b3af56c951e44fb9b49e75cc6664b01
The4thLaw <The4thLaw@users.noreply.github.com>
2016-07-06 11:24:17 +0200

    Improvements to the Integration Tests.
    - Abstracted some common methods.
    - Run them during the 'verify' phase.
    - Improve the checks for the AuthorController tests.

---
+++ source/demyo-web/src/test/java/org/demyo/web/test/mvc/AbstractMvcTest.java
@@ -0,0 +1,142 @@
+package org.demyo.web.test.mvc;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.htmlunit.MockMvcWebClientBuilder;
+import org.springframework.web.context.WebApplicationContext;
+
+import com.gargoylesoftware.htmlunit.HttpMethod;
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.WebRequest;
+import com.gargoylesoftware.htmlunit.html.HtmlAnchor;
+import com.gargoylesoftware.htmlunit.html.HtmlForm;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import com.gargoylesoftware.htmlunit.html.HtmlSubmitInput;
+import com.gargoylesoftware.htmlunit.html.HtmlTextArea;
+import com.gargoylesoftware.htmlunit.html.HtmlTextInput;
+
+/**
+ * Base class for MVC integration tests.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/demyo-context.xml")
+@WebAppConfiguration
+public abstract class AbstractMvcTest {
+	private static final Logger LOGGER = LoggerFactory.getLogger(AbstractMvcTest.class);
+
+	@Autowired
+	private WebApplicationContext wac;
+
+	/** The HTMLUnit Web Client. */
+	private WebClient webClient;
+
+	/**
+	 * Sets up the HTMLUnit Web Client.
+	 * 
+	 * @throws Exception in case of error during setup.
+	 */
+	@Before
+	public void setupWebClient() throws Exception {
+		webClient = MockMvcWebClientBuilder.webAppContextSetup(wac).build();
+	}
+
+	/**
+	 * Gets the HTMLUnit Web Client.
+	 * 
+	 * @return the HTMLUnit Web Client
+	 */
+	protected WebClient getWebClient() {
+		return webClient;
+	}
+
+	/**
+	 * Returns the main model edit form on the page.
+	 * 
+	 * @param page The page to get the form from.
+	 * @return The form, or <code>null</code> if no form was found.
+	 */
+	protected final HtmlForm getMainModelForm(HtmlPage page) {
+		List<?> forms = page.getByXPath("//form[@class='dem-model-form']");
+		if (forms.size() <= 0) {
+			LOGGER.error("No form found in specified page: {}", page.getUrl().toString());
+			return null;
+		}
+
+		Object form = forms.get(0);
+		if (!(form instanceof HtmlForm)) {
+			LOGGER.error("An element was found but is not a form in specified page: {}", page.getUrl().toString());
+			return null;
+		}
+		return (HtmlForm) form;
+	}
+
+	/**
+	 * Sets a text input on a page.
+	 * 
+	 * @param page The page.
+	 * @param fieldId The field ID.
+	 * @param value The value to set.
+	 */
+	protected final void setHtmlTextInput(HtmlPage page, String fieldId, String value) {
+		page.<HtmlTextInput> getHtmlElementById(fieldId).setValueAttribute(value);
+	}
+
+	/**
+	 * Sets a text area on a page.
+	 * 
+	 * @param page The page.
+	 * @param fieldId The field ID.
+	 * @param value The value to set.
+	 */
+	protected final void setHtmlTextArea(HtmlPage page, String fieldId, String value) {
+		page.<HtmlTextArea> getHtmlElementById(fieldId).setText(value);
+	}
+
+	/**
+	 * Submits a form.
+	 * 
+	 * @param form The form to submit.
+	 * @return The page to which the form redirects.
+	 * @throws IOException If an IO error occurs
+	 */
+	protected final HtmlPage submitForm(HtmlForm form) throws IOException {
+		HtmlSubmitInput submit = form.<HtmlSubmitInput> getOneHtmlElementByAttribute("input", "type", "submit");
+		return submit.click();
+	}
+
+	/**
+	 * Clicks a link on a page.
+	 * 
+	 * @param page The page.
+	 * @param id The link ID.
+	 * @return The page to which the link redirects.
+	 * @throws IOException If an IO error occurs
+	 */
+	protected HtmlPage clickLinkById(HtmlPage page, String id) throws IOException {
+		return page.<HtmlAnchor> getHtmlElementById("qt-edit-author").click();
+	}
+
+	/**
+	 * Deletes a model by using the delete URL given by the quick task.
+	 * 
+	 * @param page The view page for the model.
+	 * @param id The ID of the delete quicktask.
+	 * @return The page to which the action redirects.
+	 * @throws IOException If an IO error occurs
+	 */
+	protected HtmlPage deleteWithQuickTask(HtmlPage page, String id) throws IOException {
+		String deleteHref = page.<HtmlAnchor> getHtmlElementById(id).getHrefAttribute();
+		String fullDeleteUrl = page.getUrl().toString() + "/../" + deleteHref;
+		return getWebClient().getPage(new WebRequest(new URL(fullDeleteUrl), HttpMethod.POST));
+	}
+}
---
+++ source/demyo-web/src/test/java/org/demyo/web/test/mvc/AuthorContollerIT.java
@@ -0,0 +1,87 @@
+package org.demyo.web.test.mvc;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.IOException;
+import java.net.URL;
+
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+import org.demyo.web.controller.AuthorController;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.mock.jndi.SimpleNamingContextBuilder;
+
+import com.gargoylesoftware.htmlunit.HttpMethod;
+import com.gargoylesoftware.htmlunit.WebRequest;
+import com.gargoylesoftware.htmlunit.html.HtmlAnchor;
+import com.gargoylesoftware.htmlunit.html.HtmlForm;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+
+/**
+ * Integration tests for {@link AuthorController}.
+ */
+public class AuthorContollerIT extends AbstractMvcTest {
+
+	@BeforeClass
+	public static void setupDataSource() throws NamingException {
+		SimpleNamingContextBuilder builder = SimpleNamingContextBuilder.emptyActivatedContextBuilder();
+		DataSource ds = new DriverManagerDataSource("jdbc:h2:/tmp/demyo_tests;DB_CLOSE_DELAY=120;IGNORECASE=TRUE");
+		builder.bind("jdbc/demyoDataSource", ds);
+		// TODO: determine how to reset
+	}
+
+	/**
+	 * Tests an add/edit/delete sequence.
+	 * 
+	 * @throws Exception in case of error.
+	 */
+	@Test
+	public void testAll() throws Exception {
+		HtmlPage page = getWebClient().getPage("http://localhost/authors/add");
+
+		// Set some values and save
+		HtmlForm form = getMainModelForm(page);
+		setHtmlTextInput(page, "field_author_firstName", "Philippe");
+		setHtmlTextInput(page, "field_author_nickname", "Test");
+		setHtmlTextInput(page, "field_author_name", "Buchet");
+		page = submitForm(form);
+
+		// Check redirect after add
+		String viewUrl = page.getUrl().toString();
+		assertThat(viewUrl).matches(".*/authors/view/[0-9]+$");
+
+		// Check fields after add
+		String pageText = page.asText();
+		assertThat(pageText).contains("Philippe 'Test' Buchet");
+
+		// Edit
+		page = clickLinkById(page, "qt-edit-author");
+
+		// Change some values
+		form = getMainModelForm(page);
+		setHtmlTextInput(page, "field_author_website", "http://www.example.org/buchet");
+		setHtmlTextInput(page, "field_author_firstName", "Phil");
+		page = submitForm(form);
+
+		// Check redirect after edit
+		assertThat(page.getUrl().toString()).isEqualTo(viewUrl);
+
+		// Check fields after edit
+		pageText = page.asText();
+		assertThat(pageText).contains("Phil 'Test' Buchet");
+		assertThat(pageText).contains("http://www.example.org/buchet");
+
+		// Delete
+		page = deleteWithQuickTask(page, "qt-delete-author");
+
+		// Check redirect after delete
+		assertThat(page.getUrl().toString()).matches(".*/authors/$");
+
+		// TODO: this should return a 404. Currently returns a 500.
+		//getWebClient().getPage(viewUrl);
+	}
+}
