90260ce400a13366fe0f01eae4f5d66033a4e20c
The4thLaw <The4thLaw@users.noreply.github.com>
2016-08-14 16:12:06 +0200

    Using recommendations from https://github.com/joshbuchea/HEAD.
    Included a Web Application Manifest as well.

--- source/demyo-web/src/main/java/org/demyo/web/controller/HomeController.java
+++ source/demyo-web/src/main/java/org/demyo/web/controller/HomeController.java
@@ -1,14 +1,28 @@
 package org.demyo.web.controller;
 
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+
+import org.demyo.service.IConfigurationService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Controller for the home page.
  */
 @Controller
 public class HomeController extends AbstractController {
+	@Autowired
+	private MessageSource messageSource;
+	@Autowired
+	private IConfigurationService config;
+
 	/**
 	 * Displays the home page.
 	 * 
@@ -19,5 +33,29 @@ public class HomeController extends AbstractController {
 	public String viewHome(Model model) {
 		suppressQuickSearch(model);
 		return "core/home";
+	}
+
+	/**
+	 * Displays a Web Application Manifest.
+	 * 
+	 * @return The manifest map.
+	 * @see https://developer.mozilla.org/en-US/docs/Web/Manifest
+	 */
+	@RequestMapping("/manifest.json")
+	@ResponseBody
+	public Map<String, Object> getApplicationManifest() {
+		Locale language = config.getConfiguration().getLanguage();
+
+		Map<String, Object> manifest = new HashMap<>();
+		manifest.put("name", "Demyo");
+		manifest.put("short_name", "Demyo");
+		manifest.put("background_color", "#2196f3");
+		manifest.put("theme_color", "#2196f3");
+		manifest.put("description", messageSource.getMessage("app.description", null, language));
+		manifest.put("display", "standalone");
+		manifest.put("lang", language.toLanguageTag());
+		manifest.put("orientation", "portrait-primary");
+		manifest.put("start_url", "/");
+		return manifest;
 	}
 }
