9640708c03548f6c5c0718accdd75406bc452c6d
The4thLaw <The4thLaw@users.noreply.github.com>
2018-09-02 11:13:26 +0200

    Fixing HTMLUnit driver version
    Later versions depend on Java 8 Functions.
    
    Also restores a line of code ensuring an element is clickable (the part that depended on Java 8 in the newer version)
    
    Refs #5

--- source/demyo-web/src/test/java/org/demyo/web/test/mvc/AbstractMvcTest.java
+++ source/demyo-web/src/test/java/org/demyo/web/test/mvc/AbstractMvcTest.java
@@ -11,6 +11,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -303,7 +305,7 @@ public abstract class AbstractMvcTest extends AbstractPersistenceTest {
 	 */
 	protected void waitClickable(WebElement element) {
 		LOGGER.debug("Waiting for element to become clickable: {}", element);
-		// new WebDriverWait(webDriver, MAX_TIMEOUT).until(ExpectedConditions.elementToBeClickable(element));
+		new WebDriverWait(webDriver, MAX_TIMEOUT).until(ExpectedConditions.elementToBeClickable(element));
 	}
 
 	/**
