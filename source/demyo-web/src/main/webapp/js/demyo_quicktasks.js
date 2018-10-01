a946e7e494bad3ac33cf7edb5532a607e5d06ac6
The4thLaw <The4thLaw@users.noreply.github.com>
2018-10-01 16:25:18 +0200

    Coding style

--- source/demyo-web/src/main/webapp/js/demyo_quicktasks.js
+++ source/demyo-web/src/main/webapp/js/demyo_quicktasks.js
@@ -12,23 +12,26 @@ $(function() {
 			e.preventDefault();
 			
 			// Create MDL-style HTML5 dialog
-			var dialog = $('<dialog class="mdl-dialog"></dialog>')
+			var dialog = $('<dialog class="mdl-dialog"></dialog>');
 			dialog.append('<h4 class="mdl-dialog__title">' + demyo.l10n['quickTasks.confirm.title'] + '</h4>');
 			dialog.append('<div class="mdl-dialog__content"><p>' + $this.data('qt-confirm') + '</p></div>');
-			dialog.append('<div class="mdl-dialog__actions">'
-					+ '<button type="button" class="mdl-button mdl-button--primary confirm">' + demyo.l10n['quickTasks.confirm.ok.label'] + '</button>'
-					+ '<button type="button" class="mdl-button mdl-button--primary cancel">' + demyo.l10n['quickTasks.confirm.cancel.label'] + '</button>'
-					+ '</div>');
+			dialog.append('<div class="mdl-dialog__actions">' +
+					'<button type="button" class="mdl-button mdl-button--primary confirm">' +
+					demyo.l10n['quickTasks.confirm.ok.label'] + '</button>' +
+					'<button type="button" class="mdl-button mdl-button--primary cancel">' +
+					demyo.l10n['quickTasks.confirm.cancel.label'] + '</button>' +
+					'</div>');
 			
 			// Register the events
 			$('.confirm', dialog).click(function () {
 				$('<form method="post" action="'+url+'" style="display:none;"></form>')
 					.insertBefore('#page-content')
 					.submit();
-			})
+			});
 			$('.cancel', dialog).click(function () {
 				dialog.get(0).close();
-			})
+				dialog.remove();
+			});
 			
 			// Append the dialog to the body
 			$('body').append(dialog);
@@ -40,8 +43,8 @@ $(function() {
 			dialog.get(0).showModal();
 			
 			return false;
-		})
+		});
 	});
 	
-})
+});
 })(jQuery);
