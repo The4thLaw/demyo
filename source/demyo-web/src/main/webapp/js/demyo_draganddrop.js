8c3003d4125a6641dd1c456de53cba4692a1cf45
The4thLaw <The4thLaw@users.noreply.github.com>
2018-10-19 21:28:05 +0200

    Set all common labels and fix the label order and styling.
    Refs #33

--- source/demyo-web/src/main/webapp/js/demyo_draganddrop.js
+++ source/demyo-web/src/main/webapp/js/demyo_draganddrop.js
@@ -15,11 +15,31 @@ $(function () {
 			process: contextRoot + 'api/filepond/process',
 			revert: contextRoot + 'api/filepond/revert'
 		},
-		acceptedFileTypes: ['image/*']
+		acceptedFileTypes: ['image/*'],
+		// Labels
+		fileValidateTypeLabelExpectedTypes: demyo.l10n['draganddrop.filepond.fileValidateTypeLabelExpectedTypes'],
+		labelButtonAbortItemLoad: demyo.l10n['draganddrop.filepond.labelButtonAbortItemLoad'],
+		labelButtonAbortItemProcessing: demyo.l10n['draganddrop.filepond.labelButtonAbortItemProcessing'],
+		labelButtonProcessItem: demyo.l10n['draganddrop.filepond.labelButtonProcessItem'],
+		labelButtonRemoveItem: demyo.l10n['draganddrop.filepond.labelButtonRemoveItem'],
+		labelButtonRetryItemLoad: demyo.l10n['draganddrop.filepond.labelButtonRetryItemLoad'],
+		labelButtonRetryItemProcessing: demyo.l10n['draganddrop.filepond.labelButtonRetryItemProcessing'],
+		labelButtonUndoItemProcessing: demyo.l10n['draganddrop.filepond.labelButtonUndoItemProcessing'],
+		labelFileLoadError: demyo.l10n['draganddrop.filepond.labelFileLoadError'],
+		labelFileLoading: demyo.l10n['draganddrop.filepond.labelFileLoading'],
+		labelFileProcessing: demyo.l10n['draganddrop.filepond.labelFileProcessing'],
+		labelFileProcessingAborted: demyo.l10n['draganddrop.filepond.labelFileProcessingAborted'],
+		labelFileProcessingComplete: demyo.l10n['draganddrop.filepond.labelFileProcessingComplete'],
+		labelFileProcessingError: demyo.l10n['draganddrop.filepond.labelFileProcessingError'],
+		labelFileSizeNotAvailable: demyo.l10n['draganddrop.filepond.labelFileSizeNotAvailable'],
+		labelFileTypeNotAllowed: demyo.l10n['draganddrop.filepond.labelFileTypeNotAllowed'],
+		labelFileWaitingForSize: demyo.l10n['draganddrop.filepond.labelFileWaitingForSize'],
+		labelIdle: demyo.l10n['draganddrop.filepond.labelIdle'],
+		labelTapToCancel: demyo.l10n['draganddrop.filepond.labelTapToCancel'],
+		labelTapToRetry: demyo.l10n['draganddrop.filepond.labelTapToRetry'],
+		labelTapToUndo: demyo.l10n['draganddrop.filepond.labelTapToUndo']
 	});
-	// TODO: set labels: https://pqina.nl/filepond/docs/patterns/api/filepond-instance/ + https://pqina.nl/filepond/docs/patterns/plugins/file-validate-type/#properties
 	// TODO: when cancelling, revert all files
-	// TODO: set labels here
 
 	// TODO: add handler on the body element (if one of the quicktasks exists)
 	
@@ -35,7 +55,7 @@ $(function () {
 		var albumId = $('#album_id').val();
 		dialog = $('<dialog id="filepond-dialog" class="mdl-dialog"></dialog>');
 		var form = $('<form method="post" action="' + contextRoot + 'albums/' + albumId + '/filepond"></form>');
-		form.append('<h4 class="mdl-dialog__title">' + 'TODO: title' + '</h4>');
+		form.append('<h4 class="mdl-dialog__title">' + demyo.l10n['draganddrop.dialog.title'] + '</h4>');
 		form.append('<div class="mdl-dialog__content">' +
 				'<div class="dem-form-value__field">' +
 					'<label class="dem-form__label">' + 'TODO: cover' + '</label>' +
@@ -48,13 +68,13 @@ $(function () {
 			);
 		dialog.append(form);
 		dialog.append('<div class="mdl-dialog__actions">' +
-				'<button type="button" class="mdl-button mdl-button--primary confirm">' + 'TODO: OK' + '</button>' +
-				'<button type="button" class="mdl-button mdl-button--primary cancel">' + 'TODO: Cancel' + '</button>' +
+				'<button type="button" class="mdl-button mdl-button--primary cancel">' + demyo.l10n['draganddrop.button.cancel'] + '</button>' +
+				'<button type="button" class="mdl-button mdl-button--accent mdl-button--raised confirm">' + demyo.l10n['draganddrop.button.confirm'] + '</button>' +
 				'</div>');
 		
 		// Register FilePond
 		var ponds = FilePond.parse(dialog.get(0));
-		
+
 		// Register the events
 		$('.confirm', dialog).click(function () {
 			$('form', dialog).submit();
