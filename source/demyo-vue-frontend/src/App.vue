d4b9d301c373d8f605bf3fd8c27b0f91dc5a15d5
The4thLaw <The4thLaw@users.noreply.github.com>
2019-10-27 17:28:12 +0100

    Starting the base layout
    Refs #78

--- source/demyo-vue-frontend/src/App.vue
+++ source/demyo-vue-frontend/src/App.vue
@@ -1,7 +1,33 @@
 <template>
-	<v-app>
+	<v-app id="demyo">
 		<v-navigation-drawer v-model="mainMenu" app>
-			TODO: menu
+			<v-list>
+				<v-list-item href="/">
+					<v-list-item-icon>
+						<v-icon>mdi-home</v-icon>
+					</v-list-item-icon>
+					<v-list-item-content>
+						<v-list-item-title>{{ $t('menu.main.home') }}</v-list-item-title>
+					</v-list-item-content>
+				</v-list-item>
+
+				<v-list-group v-for="section in menuItems" :key="section.title" v-model="section.active">
+					<template v-slot:activator>
+						<v-list-item-content>
+							<v-list-item-title v-text="$t(section.title)" />
+						</v-list-item-content>
+					</template>
+
+					<v-list-item v-for="item in section.subItems" :key="item.title" :href="item.url">
+						<v-list-item-icon>
+							<v-icon v-text="item.icon" />
+						</v-list-item-icon>
+						<v-list-item-content>
+							<v-list-item-title v-text="item.title" />
+						</v-list-item-content>
+					</v-list-item>
+				</v-list-group>
+			</v-list>
 		</v-navigation-drawer>
 		<v-app-bar color="primary" dark app>
 			
@@ -29,8 +55,62 @@ export default {
 
 	data() {
 		return {
-			mainMenu: false
+			mainMenu: false,
+
+			menuItems: [
+				{
+					title: 'menu.section.view',
+					active: true,
+					subItems: [
+						{
+							title: 'menu.series.browse',
+							icon: 'mdi-layers-triple',
+							url: '/series'
+						},
+						{
+							title: 'menu.albums.browse',
+							icon: 'mdi-book-open-variant',
+							url: '/albums'
+						},
+						{
+							title: 'menu.authors.browse',
+							icon: 'mdi-account',
+							url: '/authors'
+						}
+					]
+				},
+				{
+					title: 'menu.section.edit',
+					subItems: [
+						{
+							title: 'menu.albums.add',
+							icon: 'mdi-book-open-variant',
+							url: '/albums/add'
+						},
+						{
+							title: 'menu.authors.add',
+							icon: 'mdi-account-plus',
+							url: '/authors/add'
+						},
+						{
+							title: 'menu.series.add',
+							icon: 'mdi-layers-plus',
+							url: '/series/add'
+						}
+					]
+				}
+			]
 		}
 	}
 }
 </script>
+
+<style lang="less">
+html[lang], #demyo {
+	font-family: -apple-system, BlinkMacSystemFont, Roboto, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+}
+
+#demyo .v-list-item__title {
+	font-size: 14px;
+}
+</style>
