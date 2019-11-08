a3606f40d08abf3c03eb7b70bfa435b242a444e4
The4thLaw <The4thLaw@users.noreply.github.com>
2019-11-08 07:41:03 +0100

    Started to paginate and group the items in the list of authors
    Refs #78

---
+++ source/demyo-vue-frontend/src/components/TextIndex.vue
@@ -0,0 +1,58 @@
+<template>
+    <div>
+        <div v-if="!splitByFirstLetter">
+            <div v-for="item in items" :key="item.id">
+                {{ firstLetterExtractor(item) }}<slot :item="item" />
+            </div>
+        </div>
+
+        <div v-if="splitByFirstLetter">
+            <div v-for="(value, key) in groupedItems" :key="key">
+                {{key}}
+                <div v-for="item in value" :key="item.id">
+                   <slot :item="item" />
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import { groupBy, deburr } from 'lodash'
+
+export default {
+    name: 'TextIndex',
+
+    props: {
+        items: {
+            type: Array,
+            required: true
+        },
+        splitByFirstLetter: {
+            type: Boolean,
+            default: true
+        },
+        firstLetterExtractor: {
+            type: Function,
+            default: () => 'A'
+        }
+    },
+
+    data() {
+        return {
+            itemsPerPage: 5,
+            currentPage: 1
+        }
+    },
+
+    computed: {
+        paginatedItems() {
+            return this.items.slice((this.currentPage-1)*this.itemsPerPage, this.currentPage*this.itemsPerPage)
+        },
+
+        groupedItems() {
+            return groupBy(this.paginatedItems, (i) => deburr(this.firstLetterExtractor(i)))
+        }
+    }
+}
+</script>
--- source/demyo-vue-frontend/src/views/authors/AuthorIndex.vue
+++ source/demyo-vue-frontend/src/views/authors/AuthorIndex.vue
@@ -1,9 +1,38 @@
 <template>
-    <div>Hello, authors</div>
+    <div>
+        <TextIndex :items="authors" :firstLetterExtractor="(item) => item.lname[0]">
+            <template v-slot:default="slotProps">{{ slotProps.item.identifyingName }}</template>
+        </TextIndex>
+    </div>
 </template>
 
 <script>
+import TextIndex from '@/components/TextIndex'
+
 export default {
-    name: 'AuthorIndex'
+    name: 'AuthorIndex',
+
+    components: {
+        TextIndex
+    },
+
+    data() {
+        return {
+            authors: [
+                {
+                    id: '1',
+                    fname: 'Jean-David',
+                    lname: 'Morvan',
+                    identifyingName: 'Jean-David Morvan'
+                },
+                {
+                    id: '2',
+                    fname: 'Jean-David 2',
+                    lname: 'Morvan 2',
+                    identifyingName: 'Jean-David 2 Morvan 2'
+                }
+            ]
+        }
+    }
 }
 </script>
