9d3f80b2a87476f9a664cbb0b87da6bdc55c2c0e
The4thLaw <The4thLaw@users.noreply.github.com>
2019-11-13 17:50:30 +0100

    Use the Spring shorthand annotations for binding REST controllers
    Refs #78

--- source/demyo-web/src/main/java/org/demyo/web/controller/api/AuthorAPIController.java
+++ source/demyo-web/src/main/java/org/demyo/web/controller/api/AuthorAPIController.java
@@ -1,13 +1,13 @@
 package org.demyo.web.controller.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import org.demyo.model.Author;
 import org.demyo.service.IAuthorService;
 
-@Controller
+@RestController
 @RequestMapping("/api/authors")
 public class AuthorAPIController extends AbstractModelAPIController<Author> {
 	private final IAuthorService service;
