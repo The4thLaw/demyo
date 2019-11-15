7f43feae1d54e619885d83aabcc6b2a2ba435e46
The4thLaw <The4thLaw@users.noreply.github.com>
2019-11-15 07:56:42 +0100

    Removed debug information and obsolete TODO from author service
    Refs #78

--- source/demyo-vue-frontend/src/services/author-service.js
+++ source/demyo-vue-frontend/src/services/author-service.js
@@ -3,9 +3,7 @@ import { apiRoot } from '@/myenv'
 
 export default {
 	async findForIndex() {
-		// TODO: must configure CORS, use a proxy
 		let response = await axios.get(apiRoot + 'authors/')
-		console.log(response.data)
 		return response.data
 	}
 }
