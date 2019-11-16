5e210fa79f530440dd740f7b2995105835567d39
The4thLaw <The4thLaw@users.noreply.github.com>
2019-11-16 17:26:43 +0100

    Implement proper i18n
    Refs #78

--- source/demyo-vue-frontend/src/i18n.js
+++ source/demyo-vue-frontend/src/i18n.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
 import VueI18n from 'vue-i18n'
+import axios from 'axios'
+import { apiRoot, defaultLanguage, fallbackLanguage } from '@/myenv'
 
 Vue.use(VueI18n)
 
+// By default, we start with partial messages covering all above-the-fold content in all supported languages
+// (mainly titles)
+// TODO: add more messages to those files (list them all)
 function loadLocaleMessages() {
 	const locales = require.context('./locales', true, /[A-Za-z0-9-_,\s]+\.json$/i)
 	const messages = {}
@@ -16,8 +21,32 @@ function loadLocaleMessages() {
 	return messages
 }
 
-export default new VueI18n({
-	locale: process.env.VUE_APP_I18N_LOCALE || 'en',
-	fallbackLocale: process.env.VUE_APP_I18N_FALLBACK_LOCALE || 'en',
+// TODO: eventually, load the Reader preferences to get the right language, and load that one and
+// the fallback. Stop relying on the navigator language.
+
+const i18n = new VueI18n({
+	locale: defaultLanguage,
+	fallbackLocale: fallbackLanguage,
 	messages: loadLocaleMessages()
 })
+console.log(`Initialized i18n with '${defaultLanguage}' as default language and '${fallbackLanguage}' as fallback`)
+
+/**
+ * Asynchronously gets translations for a given language from the server, and sets them in vue-i18n.
+ * @param {string} lang The language to get translations for
+ * @return {Promise} A Promise that always resolves to true
+ */
+async function loadLanguageFromServer(lang) {
+	let response = await axios.get(apiRoot + 'translations/' + lang)
+	i18n.setLocaleMessage(lang, response.data)
+	console.log(`Loaded ${Object.keys(response.data).length} translations from the server in ${lang}`)
+	return true
+}
+
+// i18n is already initialized and serving the critical messages in the language of choice
+// Now, we should load the full translations in the default and fallback languages
+// Load the default language, and then the fallback (prioritise the default rather than relying on luck)
+loadLanguageFromServer(defaultLanguage).then(
+	() => loadLanguageFromServer(fallbackLanguage))
+
+export default i18n
--- source/demyo-vue-frontend/src/myenv.js
+++ source/demyo-vue-frontend/src/myenv.js
@@ -5,3 +5,13 @@ if (bodyApiRoot === undefined) {
 }
 console.log('API root is', bodyApiRoot)
 export const apiRoot = bodyApiRoot
+
+// Configure the default and fallback languages
+var navigatorLanguage = navigator.language.replace(/-.*/, '')
+if (navigatorLanguage !== 'en' && navigatorLanguage !== 'fr') {
+	// We only support those two at the moment. By the time we support more, this code will have been removed
+	navigatorLanguage = 'en'
+}
+export const defaultLanguage = navigatorLanguage
+// English would have no fallback but we're sure French will remain a first-class citizen
+export const fallbackLanguage = navigatorLanguage === 'en' ? 'fr' : 'en'
