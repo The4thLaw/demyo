80ef4c2b6f84185a3e9bf41c2ab47584a427a830
The4thLaw <The4thLaw@users.noreply.github.com>
2019-11-22 17:03:34 +0100

    Improve the mapping for the image backend
    The old mapping would have prevented the regular image pages from working later.
    
    Refs #78

--- source/demyo-vue-frontend/vue.config.js
+++ source/demyo-vue-frontend/vue.config.js
@@ -5,8 +5,7 @@ module.exports = {
 		// Route requests to unknown URLs to the backend (can be JSON server or Spring)
 		proxy: {
 			'/api': { target: 'http://localhost:1607/' },
-			// TODO: check if we can refine the mapping to just take the files (something like /images/:id/file/)
-			'/images': { target: 'http://localhost:1607/' }
+			'^/images/.*/file/.*': { target: 'http://localhost:1607/' }
 		}
 	},
 
