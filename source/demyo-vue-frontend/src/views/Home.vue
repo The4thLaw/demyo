3a09c38a2c72b9b0fea22df0de6083ebcdd14867
The4thLaw <The4thLaw@users.noreply.github.com>
2019-12-07 19:57:51 +0100

    Forgot a reference of the HelloWorld component
    Refs #78

--- source/demyo-vue-frontend/src/views/Home.vue
+++ source/demyo-vue-frontend/src/views/Home.vue
@@ -3,18 +3,12 @@
 	to provide a user-accessible reset in case of issue -->
 	<div class="home">
 		<img alt="Vue logo" src="../assets/logo.png">
-		<HelloWorld msg="Welcome to Your Vue.js App" />
 	</div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
 	name: 'Home',
-	components: {
-		HelloWorld
-	}
 }
 </script>
