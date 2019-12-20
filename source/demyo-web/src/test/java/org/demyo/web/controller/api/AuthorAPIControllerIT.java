421a26fc7d6723a7dce4f46ce6eb3796c80da2c6
The4thLaw <The4thLaw@users.noreply.github.com>
2019-12-20 15:46:19 +0100

    Reworked the Java code formatter.
    We now have a better way of dealing with chained calls, which weren't so prevalent back when it was created.
    
    This may have an effect on existing files but that's going to be better on the long run.

--- source/demyo-web/src/test/java/org/demyo/web/controller/api/AuthorAPIControllerIT.java
+++ source/demyo-web/src/test/java/org/demyo/web/controller/api/AuthorAPIControllerIT.java
@@ -21,18 +21,18 @@ public class AuthorAPIControllerIT extends AbstractModelAPIIT {
 
 	@Test
 	public void index() throws Exception {
-		mockMvc.perform(get("/api/authors/")) //
-				.andExpect(status().isOk()) //
-				.andExpect(jsonPath("$", hasSize(7))) //
+		mockMvc.perform(get("/api/authors/"))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$", hasSize(7)))
 				// Check first entry. Include some checks for properties that shouldn't be mentioned
-				.andExpect(jsonPath("$[3].id").value(717)) //
-				.andExpect(jsonPath("$[3].name").value("Montlló Ribó")) //
-				.andExpect(jsonPath("$[3].firstName").value("Miquel")) //
-				.andExpect(jsonPath("$[3].nickname").doesNotExist()) //
+				.andExpect(jsonPath("$[3].id").value(717))
+				.andExpect(jsonPath("$[3].name").value("Montlló Ribó"))
+				.andExpect(jsonPath("$[3].firstName").value("Miquel"))
+				.andExpect(jsonPath("$[3].nickname").doesNotExist())
 				.andExpect(jsonPath("$[3].albumsAsWriter").doesNotExist())
 				// Check second entry. Only basic stuff
-				.andExpect(jsonPath("$[0].id").value(52)) //
-				.andExpect(jsonPath("$[0].name").value("Buchet")) //
+				.andExpect(jsonPath("$[0].id").value(52))
+				.andExpect(jsonPath("$[0].name").value("Buchet"))
 				.andExpect(jsonPath("$[0].firstName").value("Philippe"));
 	}
 }
