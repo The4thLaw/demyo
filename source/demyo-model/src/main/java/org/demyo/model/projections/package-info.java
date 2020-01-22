4a6760e345cd64b068b39d55cf69ce773efd0b66
The4thLaw <The4thLaw@users.noreply.github.com>
2020-01-22 07:26:10 +0100

    Documentation.

---
+++ source/demyo-model/src/main/java/org/demyo/model/projections/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * This package contains projections used in specific database queries.
+ * <p>
+ * Projections allow us to leverage JPA capabilities and get structured results for non-model database queries.
+ * </p>
+ */
+package org.demyo.model.projections;
