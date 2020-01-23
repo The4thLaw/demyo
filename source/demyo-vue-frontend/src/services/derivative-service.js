faefdb0e88aa4c6c6fe1e2c699d061653b0f2881
The4thLaw <The4thLaw@users.noreply.github.com>
2020-01-23 07:40:24 +0100

    Sanitize output before saving the derivative.
    We're posting an object with an ID set to "false", which Jackson doesn't like, understadably.
    
    Refs #78.

--- source/demyo-vue-frontend/src/services/derivative-service.js
+++ source/demyo-vue-frontend/src/services/derivative-service.js
@@ -7,6 +7,23 @@ class DerivativeService extends AbstractModelService {
 	constructor() {
 		super('derivatives/')
 	}
+
+	save(model) {
+		// Sanitize the model before saving
+		if (!model.series.id) {
+			model.series = {}
+		}
+		if (!model.album.id) {
+			model.album = {}
+		}
+		if (!model.artist.id) {
+			model.artist = {}
+		}
+		if (!model.source.id) {
+			model.source = {}
+		}
+		return super.save(model)
+	}
 }
 
 export default new DerivativeService()
