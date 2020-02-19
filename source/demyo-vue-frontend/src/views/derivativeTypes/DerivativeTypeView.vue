692cf3db875fa93067fbde91c93591934067aae5
The4thLaw <The4thLaw@users.noreply.github.com>
2020-02-19 07:19:23 +0100

    Streamling vue i18n $tc call
    Count is an implicit parameter.
    
    Refs #78.

--- source/demyo-vue-frontend/src/views/derivativeTypes/DerivativeTypeView.vue
+++ source/demyo-vue-frontend/src/views/derivativeTypes/DerivativeTypeView.vue
@@ -21,7 +21,7 @@
 				:to="{ name: 'DerivativeIndex', query: { withType: type.id } }"
 				color="accent" class="my-4" small outlined
 			>
-				{{ $tc('page.DerivativeType.viewDerivatives', count, {count: count}) }}
+				{{ $tc('page.DerivativeType.viewDerivatives', count) }}
 			</v-btn>
 		</SectionCard>
 	</v-container>
