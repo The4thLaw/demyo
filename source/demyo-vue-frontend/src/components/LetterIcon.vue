5d063542a5c90a7adc258205b47cdfb2ab837afe
The4thLaw <The4thLaw@users.noreply.github.com>
2020-08-16 15:49:37 +0200

    Support missing reader colour in LetterIcon.
    Refs #78.

--- source/demyo-vue-frontend/src/components/LetterIcon.vue
+++ source/demyo-vue-frontend/src/components/LetterIcon.vue
@@ -1,27 +1,40 @@
 <template>
-    <!-- Inspired from vue-letter-avatar but with a customizable color -->
-    <svg :style="'width: 24px; height: 24px; border-radius: 100%; background: ' + color + ' none repeat scroll 0% 0%;'">
-        <text x="50%" y="55%" text-anchor="middle" dominant-baseline="central"
-            style="fill: rgba(255, 255, 255, 0.7); font-family: sans-serif; font-size: 15.6px;">
-            {{ letter }}
-        </text>
-    </svg>
+	<!-- Inspired from vue-letter-avatar but with a customizable color -->
+	<svg :style="'width: 24px; height: 24px; border-radius: 100%; background: ' + actualColor + ' none repeat scroll 0% 0%;'">
+		<text
+			x="50%" y="55%" text-anchor="middle" dominant-baseline="central"
+			style="fill: rgba(255, 255, 255, 0.7); font-family: sans-serif; font-size: 15.6px;"
+		>
+			{{ letter }}
+		</text>
+	</svg>
 </template>
 
 <script>
 export default {
-    name: 'LetterIcon',
+	name: 'LetterIcon',
 
-    props: {
-        letter: {
-            type: String,
-            required: true
-        },
-        color: {
-            type: String,
-            required: true
-        }
-    }
-    
+	props: {
+		letter: {
+			type: String,
+			required: true
+		},
+		color: {
+			type: String,
+			required: true
+		}
+	},
+
+	computed: {
+		actualColor() {
+			if (this.color) {
+				return this.color
+			}
+			return this.$vuetify.theme.isDark
+				? this.$vuetify.theme.themes.dark.primary.base
+				: this.$vuetify.theme.themes.light.primary.base
+		}
+	}
+
 }
 </script>
