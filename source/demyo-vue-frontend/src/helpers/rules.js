d47980b666804af5d752b71d25f9380669ac80b3
The4thLaw <The4thLaw@users.noreply.github.com>
2020-09-27 16:40:08 +0200

    Refactoring regex-based rules and adding one for URLs.
    Refs #78

--- source/demyo-vue-frontend/src/helpers/rules.js
+++ source/demyo-vue-frontend/src/helpers/rules.js
@@ -2,14 +2,22 @@ export function mandatory(comp) {
 	return v => !!v || comp.$t('validation.mandatory')
 }
 
-export function integer(comp) {
-	// Note that this rule only works partially because browsers will return an empty value for
-	// number fields. But that's the best we can do while keeping up/down arrows.
+function regexMatch(comp, regex, key) {
 	return v => {
 		if (!v) {
 			return true
 		}
 
-		return !!String(v).match(/^[0-9]*$/) || comp.$t('validation.integer')
+		return !!String(v).match(regex) || comp.$t(key)
 	}
+}
+
+export function integer(comp) {
+	// Note that this rule only works partially because browsers will return an empty value for
+	// number fields. But that's the best we can do while keeping up/down arrows.
+	return regexMatch(comp, /^[0-9]*$/, 'validation.integer')
+}
+
+export function url(comp) {
+	return regexMatch(comp, /^https?:\/\/.*$/, 'validation.url')
 }
