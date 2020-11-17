7458071c1b838e9de18037be95b3bc6fa8e229b8
The4thLaw <The4thLaw@users.noreply.github.com>
2020-11-17 22:39:57 +0100

    Moving the PostCSS config.
    Refs #78.

---
+++ source/demyo-vue-frontend/postcss.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+	plugins: [
+		require('autoprefixer')/*,
+		TODO: try again in a while. Currently, this failed stating
+		"PostCSS plugin postcss-focus requires PostCSS 8"
+		but I was running PostCSS 8 at the time
+		require('postcss-focus')
+		*/
+	]
+}
