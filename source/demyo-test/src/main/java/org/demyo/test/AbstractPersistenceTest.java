744f8bf9d171f6ffc2f398dcf544a0d339e089ee
The4thLaw <The4thLaw@users.noreply.github.com>
2021-01-31 17:40:23 +0100

    Removed an obsolete TODO.

--- source/demyo-test/src/main/java/org/demyo/test/AbstractPersistenceTest.java
+++ source/demyo-test/src/main/java/org/demyo/test/AbstractPersistenceTest.java
@@ -7,6 +7,5 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
  * Base class for persistence tests.
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-// TODO: try to solve https://stackoverflow.com/questions/27652689/spring-test-dbunit-warning
 public abstract class AbstractPersistenceTest {
 }
