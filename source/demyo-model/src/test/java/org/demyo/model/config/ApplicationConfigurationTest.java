23bff7d36cd4e2b23facbb0d37e0923ad48c3c06
The4thLaw <The4thLaw@users.noreply.github.com>
2021-04-09 18:24:58 +0200

    Unit tests

--- source/demyo-model/src/test/java/org/demyo/model/config/ApplicationConfigurationTest.java
+++ source/demyo-model/src/test/java/org/demyo/model/config/ApplicationConfigurationTest.java
@@ -11,16 +11,31 @@ import org.junit.jupiter.api.Test;
  * Tests for the {@link ApplicationConfiguration} class.
  */
 class ApplicationConfigurationTest {
+	private static final String SAMPLE_CURRENCY = "EUR";
+
 	/**
 	 * Tests that the configuration can be loaded from a simple hard-coded set of properties.
 	 */
 	@Test
 	void testInitialisationFromStrings() {
 		Map<String, String> configStrings = new HashMap<>();
-		configStrings.put("language", "en");
+		configStrings.put(ApplicationConfiguration.CONFIG_KEY_LANGUAGE, "en");
 		configStrings.put("paging.textPageSize", "50");
 		configStrings.put("paging.imagePageSize", "25");
 		ApplicationConfiguration config = new ApplicationConfiguration(configStrings);
 		assertThat(config.getPageSizeForText()).isEqualTo(50);
+	}
+
+	/**
+	 * Tests {@link ApplicationConfiguration#asMap()}.
+	 */
+	@Test
+	void asMap() {
+		ApplicationConfiguration config = ApplicationConfiguration.getDefaultConfiguration();
+
+		config.setCurrency(SAMPLE_CURRENCY);
+
+		Map<String, String> map = config.asMap();
+		assertThat(map).containsEntry("currency", SAMPLE_CURRENCY);
 	}
 }
