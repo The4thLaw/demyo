23283d066715b2b4457df950ff93e309e36ee5ab
The4thLaw <The4thLaw@users.noreply.github.com>
2023-02-06 19:52:50 +0100

    Fix deprecation issues in spring-security
    Related to the recent dependency updates.
    
    [chore]

--- source/demyo-web/src/main/java/org/demyo/web/config/WebSecurityConfig.java
+++ source/demyo-web/src/main/java/org/demyo/web/config/WebSecurityConfig.java
@@ -2,23 +2,25 @@ package org.demyo.web.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.header.writers.XXssProtectionHeaderWriter;
 
 /**
  * Java-based configuration for Spring Security.
  */
 @EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfig {
 	private static final Logger LOGGER = LoggerFactory.getLogger(WebSecurityConfig.class);
 
 	public WebSecurityConfig() {
 		LOGGER.info("Parsing configuration");
 	}
 
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
+	@Bean
+	protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		LOGGER.debug("Configuring Spring Security");
 
 		LOGGER.debug("Configuring headers");
@@ -31,12 +33,14 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 				.contentTypeOptions()
 				// Don't care for HSTS: It's not supported by Demyo yet anyway
 				// Don't use the Spring Security CSP: it doesn't support nonces
-				.and().frameOptions().sameOrigin().xssProtection().block(true);
+				.and().frameOptions().sameOrigin().xssProtection().headerValue(XXssProtectionHeaderWriter.HeaderValue.ENABLED_MODE_BLOCK);
 
 		// Disable CSRF protection for now
 		http.csrf().disable();
 
 		LOGGER.debug("Configuring authorization");
-		http.authorizeRequests().anyRequest().permitAll();
+		http.authorizeHttpRequests().anyRequest().permitAll();
+
+		return http.build();
 	}
 }
