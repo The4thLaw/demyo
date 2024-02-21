8c92e700badd0238b8d66872293cab8f1f395ce5
The4thLaw <The4thLaw@users.noreply.github.com>
2024-02-21 22:25:51 +0100

    Move away from JNDI for the datasource.
    It will be easier to maintain and there is one less dependency on Jetty stuff. Spring manages just fine.
    
    Refs #158

--- source/demyo-app/src/main/java/org/demyo/desktop/Start.java
+++ source/demyo-app/src/main/java/org/demyo/desktop/Start.java
@@ -2,46 +2,26 @@ package org.demyo.desktop;
 
 import java.awt.GraphicsEnvironment;
 import java.awt.SplashScreen;
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
 import java.net.InetSocketAddress;
-import java.net.URLDecoder;
-import java.nio.file.Files;
 import java.nio.file.Path;
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.regex.Pattern;
 
 import javax.naming.NamingException;
 import javax.swing.JOptionPane;
 
 import org.eclipse.jetty.ee10.webapp.WebAppContext;
-import org.eclipse.jetty.plus.jndi.Resource;
 import org.eclipse.jetty.server.Server;
-import org.h2.engine.Constants;
-import org.h2.jdbcx.JdbcDataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.the4thlaw.commons.utils.h2.H2LocalUpgrader;
-import org.the4thlaw.commons.utils.h2.H2VersionManager;
 
 import org.demyo.common.config.SystemConfiguration;
 import org.demyo.common.desktop.DesktopCallbacks;
 import org.demyo.common.desktop.DesktopUtils;
-import org.demyo.common.exception.DemyoErrorCode;
-import org.demyo.common.exception.DemyoRuntimeException;
 
 /**
  * Main entry point for Demyo operation.
  */
 public final class Start {
 	private static final Logger LOGGER = LoggerFactory.getLogger(Start.class);
-	private static final String DB_USER = "demyo";
-	private static final String DB_PASSWORD = "demyo";
-	private static final String DB_FILE_SUFFIX = Pattern.quote(Constants.SUFFIX_MV_FILE) + "$";
-	/** The version of H2 used before we started tracking the version numbers. */
-	private static final int DEMYO_3_0_H2_VERSION = 196;
 
 	private Start() {
 	}
@@ -72,20 +52,13 @@ public final class Start {
 		// Try to detect the application directory, based on the app JAR (the only reliable one since others may come
 		// from the exploded WAR).
 		if (System.getProperty("demyo.applicationDirectory") == null) {
-			String path = SystemConfiguration.class.getProtectionDomain().getCodeSource().getLocation().getPath();
-			String decodedPath;
-			try {
-				decodedPath = URLDecoder.decode(path, "UTF-8");
-			} catch (UnsupportedEncodingException e) {
-				throw new DemyoRuntimeException(DemyoErrorCode.SYS_APPDIR_NOT_FOUND, e);
-			}
+			Path jarPath = Path.of(SystemConfiguration.class.getProtectionDomain().getCodeSource().getLocation().toURI());
 			// Go two directories above: from JAR to lib dir to app dir
 			System.setProperty("demyo.applicationDirectory",
-					Path.of(decodedPath).getParent().getParent().toAbsolutePath().toString());
+				jarPath.getParent().getParent().toAbsolutePath().toString());
 		}
 
-		JdbcDataSource ds = startDatabase();
-		Server server = startHttpServer(ds);
+		Server server = startHttpServer();
 
 		SystemConfiguration sysConfig = SystemConfiguration.getInstance();
 		if (sysConfig.isAutoStartWebBrowser()) {
@@ -97,7 +70,7 @@ public final class Start {
 		server.join();
 	}
 
-	private static Server startHttpServer(JdbcDataSource ds)
+	private static Server startHttpServer()
 			throws Exception {
 		SystemConfiguration sysConfig = SystemConfiguration.getInstance();
 		String httpAddress = sysConfig.getHttpAddress();
@@ -122,68 +95,11 @@ public final class Start {
 		LOGGER.info("Setting extra classpath for Jetty: {}", extraClasspath);
 		webapp.setExtraClasspath(extraClasspath);
 
-		new Resource("org.demyo.services.dataSource", ds);
 		setDesktopCallbacks(server);
 		server.setHandler(webapp);
 
 		server.start();
 		return server;
-	}
-
-	private static JdbcDataSource startDatabase()
-			throws IOException, SQLException, ReflectiveOperationException {
-		SystemConfiguration sysConfig = SystemConfiguration.getInstance();
-		Path databaseFile = sysConfig.getDatabaseFile();
-		boolean isNewDatabase = !Files.exists(databaseFile);
-		// To debug, use java -cp h2-*.jar org.h2.tools.Console
-		String databaseFilePath = databaseFile.toAbsolutePath().toString().replaceAll(DB_FILE_SUFFIX, "");
-
-		String url = "jdbc:h2:" + databaseFilePath + ";DB_CLOSE_DELAY=120;IGNORECASE=TRUE";
-		LOGGER.debug("Database URL is {}", url);
-
-		// Potentially migrate the database
-		migrateH2IfNeeded(isNewDatabase, Path.of(databaseFilePath).getParent(), url);
-
-		LOGGER.info("Starting database...");
-		JdbcDataSource ds = new JdbcDataSource();
-		ds.setURL(url);
-		ds.setUser(DB_USER);
-		ds.setPassword(DB_PASSWORD);
-
-		if (isNewDatabase) {
-			LOGGER.info("Setting the database collation...");
-			// This is the collation for French, but it should do no harm to English
-			try (Connection connec = ds.getConnection(); Statement stmt = connec.createStatement()) {
-				stmt.execute("SET DATABASE COLLATION French STRENGTH PRIMARY;");
-			}
-		}
-		return ds;
-	}
-
-	private static void migrateH2IfNeeded(boolean isNewDatabase, Path databaseFilePath, String url) throws IOException {
-		Path h2CacheDirectory;
-		String h2CacheProperty = System.getProperty("demyo.h2.cacheDirectoryName");
-		if (h2CacheProperty != null) {
-			h2CacheDirectory = Path.of(h2CacheProperty);
-		} else {
-			h2CacheDirectory = SystemConfiguration.getInstance().getApplicationDirectory()
-					.resolve("legacy-h2-versions");
-		}
-		H2LocalUpgrader upgrader = new H2LocalUpgrader(h2CacheDirectory);
-		H2VersionManager vm = new H2VersionManager(DEMYO_3_0_H2_VERSION, databaseFilePath, upgrader);
-		vm.migrateH2IfNeeded(isNewDatabase, url, DB_USER, DB_PASSWORD);
-
-		int version = vm.getCurrentVersion();
-		if (version == DEMYO_3_0_H2_VERSION) {
-			// H2 at the version of Demyo 3.0 supported stuff that it shouldn't have and on which migrations
-			// relied:
-			// - UNSIGNED INT as a datatype
-			// - Dangling commas at the end of a list of columns in a create statement
-			// - MODIFY COLUMN could be used rather than ALTER COLUMN
-			// So we need to repair because the SQL files were changes accordingly and their hashes have changed
-			LOGGER.info("Migrating from H2 {} requires a Flyway repair", version);
-			SystemConfiguration.getInstance().setFlywayRepairRequired(true);
-		}
 	}
 
 	/**
--- source/demyo-dao/src/main/java/org/demyo/dao/config/DaoConfig.java
+++ source/demyo-dao/src/main/java/org/demyo/dao/config/DaoConfig.java
@@ -1,11 +1,20 @@
 package org.demyo.dao.config;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Properties;
+import java.util.regex.Pattern;
 
 import javax.sql.DataSource;
 
 import org.flywaydb.core.Flyway;
 import org.flywaydb.core.api.configuration.ClassicConfiguration;
+import org.h2.engine.Constants;
+import org.h2.jdbcx.JdbcDataSource;
 import org.hibernate.SessionFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -20,18 +29,20 @@ import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.repository.config.BootstrapMode;
-import org.springframework.jdbc.datasource.lookup.JndiDataSourceLookup;
 import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.transaction.TransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.the4thlaw.commons.utils.h2.H2LocalUpgrader;
+import org.the4thlaw.commons.utils.h2.H2VersionManager;
 
+import org.demyo.common.config.SystemConfiguration;
 import org.demyo.common.exception.DemyoErrorCode;
 import org.demyo.common.exception.DemyoRuntimeException;
 import org.demyo.dao.utils.FlywayUtils;
 
 /**
- * Configuration for the DAO layer.
+ * Configuration for the DAO layer (mainly database and migrations).
  */
 @Configuration
 @ComponentScan("org.demyo")
@@ -40,7 +51,14 @@ import org.demyo.dao.utils.FlywayUtils;
 @EnableTransactionManagement
 public class DaoConfig {
 	private static final Logger LOGGER = LoggerFactory.getLogger(DaoConfig.class);
+	private static final String DS_BEAN_NAME = "dataSource";
 	private static final String FLYWAY_BEAN_NAME = "flyway";
+
+	private static final String DB_USER = "demyo";
+	private static final String DB_PASSWORD = "demyo";
+	private static final String DB_FILE_SUFFIX = Pattern.quote(Constants.SUFFIX_MV_FILE) + "$";
+	/** The version of H2 used before we started tracking the version numbers. */
+	private static final int DEMYO_3_0_H2_VERSION = 196;
 
 	@Value("${config.hibernate.debug}")
 	private String hibernateDebug;
@@ -49,14 +67,65 @@ public class DaoConfig {
 		LOGGER.info("Parsing configuration");
 	}
 
-	@Bean(name = "dataSource")
-	public DataSource dataSource() {
-		JndiDataSourceLookup lookup = new JndiDataSourceLookup();
-		return lookup.getDataSource("org.demyo.services.dataSource");
+	@Bean(name = DS_BEAN_NAME)
+	public DataSource dataSource() throws IOException, SQLException {
+		SystemConfiguration sysConfig = SystemConfiguration.getInstance();
+		Path databaseFile = sysConfig.getDatabaseFile();
+		boolean isNewDatabase = !Files.exists(databaseFile);
+		// To debug, use java -cp h2-*.jar org.h2.tools.Console
+		String databaseFilePath = databaseFile.toAbsolutePath().toString().replaceAll(DB_FILE_SUFFIX, "");
+
+		String url = "jdbc:h2:" + databaseFilePath + ";DB_CLOSE_DELAY=120;IGNORECASE=TRUE";
+		LOGGER.debug("Database URL is {}", url);
+
+		// Potentially migrate the database
+		migrateH2IfNeeded(isNewDatabase, Path.of(databaseFilePath).getParent(), url);
+
+		LOGGER.info("Starting database...");
+		JdbcDataSource ds = new JdbcDataSource();
+		ds.setURL(url);
+		ds.setUser(DB_USER);
+		ds.setPassword(DB_PASSWORD);
+
+		if (isNewDatabase) {
+			LOGGER.info("Setting the database collation...");
+			// This is the collation for French, but it should do no harm to English
+			try (Connection connec = ds.getConnection(); Statement stmt = connec.createStatement()) {
+				stmt.execute("SET DATABASE COLLATION French STRENGTH PRIMARY;");
+			}
+		}
+		return ds;
+	}
+	private static void migrateH2IfNeeded(boolean isNewDatabase, Path databaseFilePath, String url) throws IOException {
+		Path h2CacheDirectory;
+		String h2CacheProperty = System.getProperty("demyo.h2.cacheDirectoryName");
+		if (h2CacheProperty != null) {
+			h2CacheDirectory = Path.of(h2CacheProperty);
+		} else {
+			h2CacheDirectory = SystemConfiguration.getInstance().getApplicationDirectory()
+					.resolve("legacy-h2-versions");
+		}
+		H2LocalUpgrader upgrader = new H2LocalUpgrader(h2CacheDirectory);
+		H2VersionManager vm = new H2VersionManager(DEMYO_3_0_H2_VERSION, databaseFilePath, upgrader);
+		vm.migrateH2IfNeeded(isNewDatabase, url, DB_USER, DB_PASSWORD);
+
+		int version = vm.getCurrentVersion();
+		if (version == DEMYO_3_0_H2_VERSION) {
+			// H2 at the version of Demyo 3.0 supported stuff that it shouldn't have and on which migrations
+			// relied:
+			// - UNSIGNED INT as a datatype
+			// - Dangling commas at the end of a list of columns in a create statement
+			// - MODIFY COLUMN could be used rather than ALTER COLUMN
+			// So we need to repair because the SQL files were changes accordingly and their hashes have changed
+			LOGGER.info("Migrating from H2 {} requires a Flyway repair", version);
+			SystemConfiguration.getInstance().setFlywayRepairRequired(true);
+		}
 	}
 
 	@Bean(name = FLYWAY_BEAN_NAME, initMethod = "migrate")
-	public Flyway flyway() {
+	// Must be run after the Datasource to be sure that a potential repair has been flagged
+	@DependsOn(DS_BEAN_NAME)
+	public Flyway flyway() throws IOException, SQLException {
 		ClassicConfiguration conf = new ClassicConfiguration();
 		// Old Demyo databases may have schema_version, but that is no longer the default since Flyway 5
 		conf.setTable("schema_version");
@@ -72,7 +141,7 @@ public class DaoConfig {
 	@Bean(name = "entityManagerFactory")
 	// Must be run after Flyway to ensure the database is compatible with the code
 	@DependsOn(FLYWAY_BEAN_NAME)
-	public LocalSessionFactoryBean entityManagerFactory() {
+	public LocalSessionFactoryBean entityManagerFactory() throws IOException, SQLException {
 		LocalSessionFactoryBean factory = new LocalSessionFactoryBean();
 		factory.setPackagesToScan("org.demyo.model", "org.demyo.model.*", "org.demyo.model.**.*");
 		factory.setDataSource(dataSource());
@@ -97,7 +166,7 @@ public class DaoConfig {
 	 * https://github.com/spring-projects/spring-framework/issues/13599#issuecomment-453364266
 	 */
 	@Bean("transactionManager")
-	public TransactionManager transactionManager() {
+	public TransactionManager transactionManager() throws IOException, SQLException {
 		SessionFactory sessionFactory = entityManagerFactory().getObject();
 		if (sessionFactory == null) {
 			throw new DemyoRuntimeException(DemyoErrorCode.SYS_STARTUP_ERROR, "The session factory is null");
@@ -106,7 +175,7 @@ public class DaoConfig {
 	}
 
 	@Bean
-	public BeanPostProcessor exceptionTranslator() {
+	public static BeanPostProcessor exceptionTranslator() {
 		return new PersistenceExceptionTranslationPostProcessor();
 	}
 
