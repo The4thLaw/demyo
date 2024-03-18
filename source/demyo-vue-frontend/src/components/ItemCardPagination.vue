093ee3851639ae9306df2930829803373b370bbf
The4thLaw <The4thLaw@users.noreply.github.com>
2024-03-18 20:53:30 +0100

    Fixed card pagination.
    Refs #156

--- source/demyo-vue-frontend/src/components/ItemCardPagination.vue
+++ source/demyo-vue-frontend/src/components/ItemCardPagination.vue
@@ -1,10 +1,10 @@
 <template>
 	<v-list-item v-if="pageCount > 1" class="c-ItemCardPagination">
 		<!-- Custom pagination because vuetify doesn't provide no-count pagination -->
-		<v-btn :disabled="!hasPreviousPage" text icon @click.stop="$emit('prev-page')">
+		<v-btn :disabled="!hasPreviousPage" variant="text" icon @click.stop="$emit('prev-page')">
 			<v-icon>mdi-chevron-left</v-icon>
 		</v-btn>
-		<v-btn :disabled="!hasNextPage" text icon @click.stop="$emit('next-page')">
+		<v-btn :disabled="!hasNextPage" variant="text" icon @click.stop="$emit('next-page')">
 			<v-icon>mdi-chevron-right</v-icon>
 		</v-btn>
 	</v-list-item>
@@ -34,12 +34,15 @@ export default {
 </script>
 
 <style lang="less">
-// Align the pagination buttons
+// Align the pagination buttons: have the pagination take the full width,
+// its content as well, and the buttons spaced evenly
 .c-ItemCardPagination {
-	justify-content: space-around;
+	display: flex;
 
-	&.v-list-item::after {
-		display: none;
+	.v-list-item__content {
+		flex: 1;
+		display: flex;
+		justify-content: space-around;
 	}
 }
 </style>
