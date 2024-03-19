d429f0364167e931d968988ba47a6cb4b9314c61
The4thLaw <The4thLaw@users.noreply.github.com>
2024-03-19 22:36:24 +0100

    Fixed the search results styling.
    Refs #156

--- source/demyo-vue-frontend/src/components/QuickSearchResults.vue
+++ source/demyo-vue-frontend/src/components/QuickSearchResults.vue
@@ -3,35 +3,33 @@
 		<template v-if="hasResults">
 			<template v-for="key in modelTypes">
 				<template v-if="results[key]">
-					<h2 :key="`ti-${key}-title`" class="text-subtitle-1 primary--text">
+					<h2 :key="`ti-${key}-title`" class="text-subtitle-1 text-primary">
 						{{ $t('quicksearch.results.title.' + key) }}
 					</h2>
-					<v-list :key="`ti-${key}`" class="dem-columnized c-QuickSearchResults__list" dense>
+					<v-list :key="`ti-${key}`" class="dem-columnized c-QuickSearchResults__list" density="compact">
 						<v-list-item v-for="item in results[key]" :key="item.id">
-							<v-list-item-content>
-								<div>
-									<router-link :to="`/${key}/${item.id}/view`" @click="$emit('click')">
-										<template v-if="key !== 'albums'">
-											{{ item.identifyingName }}
-										</template>
-										<template v-else>
-											{{ item.title }}
-										</template>
-									</router-link>
-								</div>
-								<div v-if="key === 'albums' && item.series" class="c-QuickSearchResults__albumSeries">
-									<router-link :to="`/series/${item.series.id}/view`" @click="$emit('click')">
-										{{ item.series.identifyingName }}
-									</router-link>
-								</div>
-							</v-list-item-content>
+							<div>
+								<router-link :to="`/${key}/${item.id}/view`" @click="$emit('navigate')">
+									<template v-if="key !== 'albums'">
+										{{ item.identifyingName }}
+									</template>
+									<template v-else>
+										{{ item.title }}
+									</template>
+								</router-link>
+							</div>
+							<div v-if="key === 'albums' && item.series" class="c-QuickSearchResults__albumSeries">
+								<router-link :to="`/series/${item.series.id}/view`" @click="$emit('navigate')">
+									{{ item.series.identifyingName }}
+								</router-link>
+							</div>
 						</v-list-item>
 					</v-list>
 				</template>
 			</template>
 		</template>
 		<template v-else-if="hasLoadedResults">
-			<v-alert border="left" type="info" text class="my-4">
+			<v-alert border="start" type="info" text class="my-4">
 				{{ $t('quicksearch.noResults') }}
 			</v-alert>
 		</template>
