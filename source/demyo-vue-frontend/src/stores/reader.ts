a3fd9a5c387c4e63bd94bfd9e444d69cbc12477b
The4thLaw <The4thLaw@users.noreply.github.com>
2024-03-24 18:17:33 +0100

    Migrated all Pinia config to TypeScript
    Refs #156

--- source/demyo-vue-frontend/src/stores/reader.ts
+++ source/demyo-vue-frontend/src/stores/reader.ts
@@ -2,13 +2,22 @@ import sortedIndex from 'lodash/sortedIndex'
 import sortedIndexOf from 'lodash/sortedIndexOf'
 import { defineStore } from 'pinia'
 
+interface ReaderLists {
+	/** The IDs of the reader's favourite series. */
+	favouriteSeries: number[]
+	/** The IDs of the reader's favourite albums. */
+	favouriteAlbums: number[]
+	/** The IDs of the albums in the reader's reading list */
+	readingList: number[]
+}
+
 /**
  * Inserts an item at the right position in a sorted array.
  *
- * @param {Array<Number>} list The array to modify
- * @param {Number} item The item to insert
+ * @param list The array to modify
+ * @param item The item to insert
  */
-function insert(list, item) {
+function insert(list: number[], item: number) {
 	if (sortedIndexOf(list, item) >= 0) {
 		// Don't add twice
 		return
@@ -20,10 +29,10 @@ function insert(list, item) {
 /**
  * Removes an item from a sorted array.
  *
- * @param {Array<Number>} list The array to modify
- * @param {Number} item The item to remove
+ * @param list The array to modify
+ * @param item The item to remove
  */
-function remove(list, item) {
+function remove(list: number[], item: number) {
 	const index = sortedIndexOf(list, item)
 	if (index >= 0) {
 		list.splice(index, 1)
@@ -32,10 +41,10 @@ function remove(list, item) {
 
 /**
  * Pre-processes a list for assignment
- * @param {Array<Number>} list The list to modify
- * @return {Array<Number>} The modified list
+ * @param list The list to modify
+ * @return The modified list
  */
-function preprocess(list) {
+function preprocess(list: number[]): number[] {
 	list = list || []
 	return list.sort((a, b) => a - b)
 }
@@ -45,15 +54,16 @@ export const useReaderStore = defineStore('reader', {
 	state: () => ({
 		currentReader: {},
 		// Sets would be nice, but Vue won't support reactive sets until sometime in v3
-		favouriteSeries: [],
-		favouriteAlbums: [],
-		readingList: [],
+		favouriteSeries: [] as number[],
+		favouriteAlbums: [] as number[],
+		readingList: [] as number[],
 		readerLoaded: false,
 		readerSelectionRequired: false
 	}),
 
 	actions: {
-		setCurrentReader(reader) {
+		// TODO: TypeScript: define a type for the reader
+		setCurrentReader(reader: any) {
 			this.currentReader = reader
 			this.readerSelectionRequired = false
 			this.readerLoaded = true
@@ -64,37 +74,37 @@ export const useReaderStore = defineStore('reader', {
 			this.readerLoaded = false
 		},
 
-		setReaderLists(lists) {
+		setReaderLists(lists: ReaderLists) {
 			this.favouriteSeries = preprocess(lists.favouriteSeries)
 			this.favouriteAlbums = preprocess(lists.favouriteAlbums)
 			this.readingList = preprocess(lists.readingList)
 		},
 
-		setReadingList(list) {
+		setReadingList(list: number[]) {
 			this.readingList = preprocess(list)
 		},
 
-		addFavouriteSeries(item) {
+		addFavouriteSeries(item: number) {
 			insert(this.favouriteSeries, item)
 		},
 
-		removeFavouriteSeries(item) {
+		removeFavouriteSeries(item: number) {
 			remove(this.favouriteSeries, item)
 		},
 
-		addFavouriteAlbum(item) {
+		addFavouriteAlbum(item: number) {
 			insert(this.favouriteAlbums, item)
 		},
 
-		removeFavouriteAlbum(item) {
+		removeFavouriteAlbum(item: number) {
 			remove(this.favouriteAlbums, item)
 		},
 
-		addToReadingList(item) {
+		addToReadingList(item: number) {
 			insert(this.readingList, item)
 		},
 
-		removeFromReadingList(item) {
+		removeFromReadingList(item: number) {
 			remove(this.readingList, item)
 		}
 	}
