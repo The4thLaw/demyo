db6a4551a6d0527c760674eeb42bf02f021bb14c
The4thLaw <The4thLaw@users.noreply.github.com>
2024-09-22 23:05:27 +0200

    Migrated the image helper to TypeScript.
    Refs #156

--- source/demyo-vue-frontend/src/helpers/images.ts
+++ source/demyo-vue-frontend/src/helpers/images.ts
@@ -1,12 +1,15 @@
 import { contextRoot } from '@/myenv'
 
-export function getEncodedImageName(image) {
+export function getEncodedImageName(image: Image) {
 	let encodedName = encodeURI(image.userFileName)
 	// The # isn't url-encoded but can cause issues in e.g. srcset attributes
 	encodedName = encodedName.replace(/#/, '')
 	return encodedName
 }
 
-export function getBaseImageUrl(image) {
+export function getBaseImageUrl(image: Image) {
+	if (!image.id) {
+		return null
+	}
 	return `${contextRoot}images/${image.id}/file/${getEncodedImageName(image)}`
 }
