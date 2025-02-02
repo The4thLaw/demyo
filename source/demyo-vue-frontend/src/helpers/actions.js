c6f0a5d1dcdd26a121ef3edc04ceb61b383aebdc
The4thLaw <The4thLaw@users.noreply.github.com>
2025-02-02 18:08:35 +0100

    Fix missing i18n reference.
    Refs #156.

--- source/demyo-vue-frontend/src/helpers/actions.js
+++ source/demyo-vue-frontend/src/helpers/actions.js
@@ -3,27 +3,6 @@ import router from '@/router'
 import { useUiStore } from '@/stores/ui'
 
 /**
- * Stub for a save action in an edit component.
- * @param {Vue} component The Vue component
- * @param {Function} handler The function that will perform the save
- * @param {String} routeName The named route to redirect to on success
- */
-export async function saveStub(component, handler, routeName) {
-	if (!component.$refs.form.validate()) {
-		return
-	}
-	const uiStore = useUiStore()
-	uiStore.enableGlobalOverlay()
-	const id = await handler()
-	uiStore.disableGlobalOverlay()
-	if (id <= 0) {
-		uiStore.showSnackbar(i18n.t('core.exception.api.title'))
-	} else {
-		router.push({ name: routeName, params: { id: id } })
-	}
-}
-
-/**
  * Stub for a delete action in a view component.
  * @param {Vue} component The Vue component
  * @param {Function} handler The function that will perform the deletion
@@ -35,7 +14,7 @@ export async function deleteStub(component, handler, confirmationLabel, routeNam
 	const deleted = await handler()
 	if (deleted) {
 		const uiStore = useUiStore()
-		uiStore.showSnackbar(i18n.t(confirmationLabel))
+		uiStore.showSnackbar(i18n.global.t(confirmationLabel))
 		router.push({ name: routeName })
 	}
 }
