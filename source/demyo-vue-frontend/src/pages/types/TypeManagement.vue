94dada275986260a44d50322e1fa24d33759cb8f
The4thLaw <The4thLaw@users.noreply.github.com>
2025-04-27 18:19:17 +0200

    Nearly finished type management.
    Refs #210

--- source/demyo-vue-frontend/src/pages/types/TypeManagement.vue
+++ source/demyo-vue-frontend/src/pages/types/TypeManagement.vue
@@ -1,58 +1,55 @@
 <template>
 	<div id="v-TypeManagement">
-		<SectionCard :title="$t('title.index.binding')">
-			TODO
+		<SectionCard :title="$t('title.index.binding')" :loading="bindingsLoading">
+			<TypeIndex
+				view-route="BindingView" edit-route="BindingEdit"
+				:items="bindings"
+			/>
 		</SectionCard>
 
-		<SectionCard :title="$t('title.index.derivativeType')">
-			TODO
+		<SectionCard :title="$t('title.index.derivativeType')" :loading="derivativeTypesLoading">
+			<TypeIndex
+				view-route="DerivativeTypeView" edit-route="DerivativeTypeEdit"
+				:items="derivativeTypes"
+			/>
 		</SectionCard>
 
 		<SectionCard :title="$t('title.index.bookType')" :loading="bookTypeManagement && bookTypesLoading">
 			<template v-if="!bookTypeManagement">
 				<v-alert border="start" type="info" text class="my-4">
 					{{ $t('page.BookType.management.disabled') }}
-
 				</v-alert>
 				<v-btn color="secondary" @click="enableBookTypeManagement">
 					{{ $t('page.BookType.management.enable') }}
 				</v-btn>
 			</template>
-			<template v-else>
-				<v-data-table :items="bookTypes" :headers="headers" :hide-default-footer="bookTypes.length < 11">
-					<template v-slot:item.actions="{ item }">
-						<v-btn :to="`/bookTypes/${item.id}/edit`" icon variant="text">
-							<v-icon>mdi-pencil</v-icon>
-						</v-btn>
-						<!-- TODO: delete and reassign -->
-						<v-btn :to="`/bookTypes/${item.id}/edit`" icon variant="text">
-							<v-icon>mdi-delete</v-icon>
-						</v-btn>
-					</template>
-				</v-data-table>
-			</template>
+			<TypeIndex
+				v-else
+				edit-route="BookTypeEdit" :items="bookTypes" deletable
+				@delete="deleteBookType"
+			/>
 		</SectionCard>
 
 		<v-speed-dial
 			location="bottom center"
 		>
-			<template v-slot:activator="{ props: activatorProps }">
+			<template #activator="{ props: activatorProps }">
 				<!-- Vuetify's own FAB doesn't work for some reason: the options overlay the base button -->
-				<Fab v-bind="activatorProps" icon="mdi-plus"></Fab>
+				<Fab v-bind="activatorProps" icon="mdi-plus" />
 			</template>
 
 			<v-tooltip key="1" :text="$t('menu.bindings.add')">
-				<template v-slot:activator="{ props }">
+				<template #activator="{ props }">
 					<v-btn key="1" to="/bindings/new" icon="mdi-notebook" v-bind="props" />
 				</template>
 			</v-tooltip>
 			<v-tooltip key="2" :text="$t('menu.derivative_types.add')">
-				<template v-slot:activator="{ props }">
+				<template #activator="{ props }">
 					<v-btn key="1" to="/derivativeTypes/new" icon="mdi-brush" v-bind="props" />
 				</template>
 			</v-tooltip>
 			<v-tooltip key="3" :text="$t('menu.book_types.add')">
-				<template v-slot:activator="{ props }">
+				<template #activator="{ props }">
 					<v-btn key="1" to="/bookTypes/new" icon="mdi-bookshelf" v-bind="props" />
 				</template>
 			</v-tooltip>
@@ -61,31 +58,47 @@
 </template>
 
 <script setup lang="ts">
+import bindingService from '@/services/binding-service'
 import bookTypeService from '@/services/book-type-service'
+import derivativeTypeService from '@/services/derivative-type-service'
 import { useRefDataStore } from '@/stores/ref-data'
-import { useI18n } from 'vue-i18n'
 
-const i18n = useI18n()
+// Bindings
+const bindings = ref([] as Binding[])
+const bindingsLoading = ref(true)
+void (async (): Promise<void> => {
+	bindings.value = await bindingService.findForIndex()
+	bindingsLoading.value = false
+})()
+
+// Derivative types
+const derivativeTypes = ref([] as DerivativeType[])
+const derivativeTypesLoading = ref(true)
+void (async (): Promise<void> => {
+	derivativeTypes.value = await derivativeTypeService.findForIndex()
+	derivativeTypesLoading.value = false
+})()
+
+// Book types
 const refDataStore = useRefDataStore()
-
 const bookTypeManagement = computed(() => refDataStore.bookTypeManagement)
-
-const headers = [
-	{ title: i18n.t('field.BookType.name'), key: 'identifyingName' },
-	{ title: i18n.t('page.TypeManagement.actions'), key: 'actions', align: 'end', sortable: false }
-]
-
 
 const bookTypes = ref([] as BookType[])
 const bookTypesLoading = ref(true)
-async function loadBookTypes() {
+async function loadBookTypes(): Promise<void> {
 	bookTypes.value = await bookTypeService.findForIndex()
 	bookTypesLoading.value = false
 }
-loadBookTypes()
+void loadBookTypes()
 
-async function enableBookTypeManagement() {
+async function enableBookTypeManagement(): Promise<void> {
 	await bookTypeService.enableManagement()
-	refDataStore.refreshBookTypeManagement()
+	await refDataStore.refreshBookTypeManagement()
+}
+
+function deleteBookType(): void {
+	// TODO: delete and reassign
+	// Reload
+	void loadBookTypes()
 }
 </script>
