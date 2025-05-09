3c375f79ae8420e247d1b3d45bc4e9b3f8d8271a
The4thLaw <The4thLaw@users.noreply.github.com>
2025-05-09 16:12:08 +0200

    Update PublisherIndex.vue
    Use speed dials for the publisher index page.
    
    Allow creating new publishers and collections (previously only publishers).
    
    Refs #203

--- source/demyo-vue-frontend/src/pages/publishers/PublisherIndex.vue
+++ source/demyo-vue-frontend/src/pages/publishers/PublisherIndex.vue
@@ -8,7 +8,24 @@
 				<PublisherCard :publisher="slotProps.item" />
 			</template>
 		</CardTextIndex>
-		<Fab to="/publishers/new" icon="mdi-plus" />
+
+		<v-speed-dial
+			location="bottom center"
+		>
+			<template #activator="{ props: activatorProps }">
+				<!-- Vuetify's own FAB doesn't work for some reason: the options overlay the base button -->
+				<Fab v-bind="activatorProps" icon="mdi-plus" />
+			</template>
+
+			<v-btn key="1" to="/publishers/new" icon>
+				<v-icon>mdi-office-building</v-icon>
+				<v-tooltip :text="$t('menu.publishers.add')" activator="parent" />
+			</v-btn>
+			<v-btn key="2" to="/collections/new" icon>
+				<v-icon>mdi-folder-multiple</v-icon>
+				<v-tooltip :text="$t('menu.collections.add')" activator="parent" />
+			</v-btn>
+		</v-speed-dial>
 	</div>
 </template>
 
