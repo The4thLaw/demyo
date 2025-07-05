7f6b81cd6ef8e687bb3ffa6e5f141901926f50f8
The4thLaw <The4thLaw@users.noreply.github.com>
2025-07-05 09:03:25 +0200

    Migrate DerivativeSource & Tag light creation to the new named abstraction.
    Refs #117.

--- source/demyo-vue-frontend/src/components/derivativeSources/DerivativeSourceLightCreate.vue
+++ source/demyo-vue-frontend/src/components/derivativeSources/DerivativeSourceLightCreate.vue
@@ -1,25 +1,12 @@
 <template>
-	<v-container fluid>
-		<v-form ref="form">
-			<v-row>
-				<v-col cols="12">
-					<v-text-field
-						v-model="source.name" :label="$t('field.DerivativeSource.name')"
-						:rules="rules.name" required @keydown.enter.stop.prevent="saveAndEmit"
-					/>
-				</v-col>
-			</v-row>
-			<Teleport defer :disabled="!teleportActions" :to="teleportActions">
-				<FormActions v-if="!loading" :show-reset="false" :show-back="false" @save="saveAndEmit" />
-			</Teleport>
-		</v-form>
-	</v-container>
+	<NamedLightCreate
+		:service="derivativeSourceService" :teleport-actions="teleportActions" model-name="Tag"
+		@save="(id: number) => emit('save', id)"
+	/>
 </template>
 
 <script setup lang="ts">
-import { useLightEdit } from '@/composables/model-edit'
-import { mandatory } from '@/helpers/rules'
-import sourceService from '@/services/derivative-source-service'
+import derivativeSourceService from '@/services/derivative-source-service'
 
 defineProps<{
 	teleportActions?: string
@@ -27,12 +14,4 @@ defineProps<{
 const emit = defineEmits<{
 	save: [id: number]
 }>()
-
-const { model: source, loading, saveAndEmit } = useLightEdit(async () => Promise.resolve({}), sourceService, emit)
-
-const rules = {
-	name: [
-		mandatory()
-	]
-}
 </script>
--- source/demyo-vue-frontend/src/components/tags/TagLightCreate.vue
+++ source/demyo-vue-frontend/src/components/tags/TagLightCreate.vue
@@ -1,25 +1,11 @@
 <template>
-	<v-container fluid>
-		<v-form ref="form">
-			<v-row>
-				<v-col cols="12">
-					<v-text-field
-						v-model="tag.name" :label="$t('field.Tag.name')" :rules="rules.name" required
-						@keydown.enter.stop.prevent="saveAndEmit"
-					/>
-				</v-col>
-			</v-row>
-
-			<Teleport defer :disabled="!teleportActions" :to="teleportActions">
-				<FormActions v-if="!loading" :show-reset="false" :show-back="false" @save="saveAndEmit" />
-			</Teleport>
-		</v-form>
-	</v-container>
+	<NamedLightCreate
+		:service="tagService" :teleport-actions="teleportActions" model-name="Tag"
+		@save="(id: number) => emit('save', id)"
+	/>
 </template>
 
 <script setup lang="ts">
-import { useLightEdit } from '@/composables/model-edit'
-import { mandatory } from '@/helpers/rules'
 import tagService from '@/services/tag-service'
 
 defineProps<{
@@ -28,12 +14,4 @@ defineProps<{
 const emit = defineEmits<{
 	save: [id: number]
 }>()
-
-const { model: tag, loading, saveAndEmit } = useLightEdit(async () => Promise.resolve({}), tagService, emit)
-
-const rules = {
-	name: [
-		mandatory()
-	]
-}
 </script>
