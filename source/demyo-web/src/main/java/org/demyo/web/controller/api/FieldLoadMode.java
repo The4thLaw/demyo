9f334423cc619415b3038d497405198da39e7bef
The4thLaw <The4thLaw@users.noreply.github.com>
2025-08-06 20:50:56 +0200

    Allow differentiating between view and edit methods when fetching data via the API.
    Refs #253

---
+++ source/demyo-web/src/main/java/org/demyo/web/controller/api/FieldLoadMode.java
@@ -0,0 +1,21 @@
+package org.demyo.web.controller.api;
+
+import java.util.Locale;
+
+/**
+ * Defines the way model fields are loaded in the API.
+ */
+public enum FieldLoadMode {
+	/** Automatic field loading. May involve post-processing. */
+	AUTO,
+	/** Raw field loading. Non-essential post-processing is bypassed. */
+	RAW;
+
+	/**
+	 * Similar to {@link #valueOf(String)}, but ignoring the case of the input value.
+	 */
+	public static FieldLoadMode valueOfIgnoreCase(String value) {
+		value = value.toUpperCase(Locale.ROOT);
+		return valueOf(value);
+	}
+}
