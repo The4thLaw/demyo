f8a41ef012fa38aa4115efe1ad98c202bc760362
The4thLaw <The4thLaw@users.noreply.github.com>
2025-10-18 20:53:26 +0200

    Log when Demyo is ready.
    Refs #205.

--- source/demyo-app/src/main/java/org/demyo/desktop/Start.java
+++ source/demyo-app/src/main/java/org/demyo/desktop/Start.java
@@ -9,6 +9,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 import org.demyo.common.desktop.DesktopCallbacks;
 
@@ -28,7 +29,15 @@ public final class Start {
 	 * @param args The command line arguments.
 	 */
 	public static void main(String[] args) {
-		SpringApplication.run(Start.class, args);
+		ConfigurableApplicationContext ctx = SpringApplication.run(Start.class, args);
+		String host = ctx.getEnvironment().getProperty("server.address");
+		if (host == null) {
+			// TODO: #205: Actually this should be 0.0.0.0 so we should log all addresses.
+			host = "127.0.0.1";
+		}
+		String port = ctx.getEnvironment().getProperty("server.port");
+		String context = ctx.getEnvironment().getProperty("server.servlet.context-path");
+		LOGGER.info("Demyo is now ready on http://{}:{}{}", host, port, context);
 	}
 
 	/**
