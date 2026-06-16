93bd5a2b648bc0c186eceb1f99b24fa2c918afbc
The4thLaw <The4thLaw@users.noreply.github.com>
2026-06-16 22:38:28 +0200

    Fix export of universe images.

--- source/demyo-service/src/main/java/org/demyo/service/exporting/Demyo2Exporter.java
+++ source/demyo-service/src/main/java/org/demyo/service/exporting/Demyo2Exporter.java
@@ -83,7 +83,11 @@ public class Demyo2Exporter implements IExporter {
 			exportModel(xsw, "authors", "author", "AUTHORS");
 			exportModel(xsw, "taxons", "taxon", "TAXONS");
 			exportModel(xsw, "book_types", "book_type", "BOOK_TYPES");
-			exportModel(xsw, "universes", "universe", "UNIVERSES");
+
+			exportModel(xsw, "universes", "universe", "UNIVERSES",
+					new ManyToManyRelation("universe-images", "universe-image", "UNIVERSE_ID", "IMAGE_ID",
+							rawSqlDao.getRawRecords("UNIVERSES_IMAGES")));
+
 			exportModel(xsw, "series-list", "series", "SERIES",
 					new ManyToManyRelation("series-taxons", "series-taxon", SERIES_KEY, "TAXON_ID",
 							rawSqlDao.getRawRecords("SERIES_TAXONS")));
